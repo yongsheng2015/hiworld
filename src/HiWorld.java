cfe51f1dcff84895fdab0d46f594b915071ad404
yongsheng5419 <yongsheng5419@163.com>
2018-04-21 13:42:00 +0800

    test01 in IDEA update02

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,5 @@
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("Hi World!随意改改！！hzw");
+        System.out.println("Hi World!随意改改！！hzw！！");
     }
 }
